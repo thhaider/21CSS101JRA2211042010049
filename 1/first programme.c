4357583d9fd44918e8890fc02c39697cc9972afa
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-03 10:34:04 +0530

    Create first programme.c

---
+++ 1/first programme.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+ int main()
+ 
+ {
+     printf("Name         : Haider Jeelani Shah \nProgramme    : B.tech.(CS&BS)\nRegister No. : RA2211042010049 \nValid From   : Jun-2022 To May-2026");
+
+     return 0;
+ }
