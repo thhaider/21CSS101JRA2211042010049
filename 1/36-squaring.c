a971e6ad5e07f9e90f3e41c4d72904c9573c7d4d
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 17:09:23 +0530

    Update and rename squaring.c (36) to 36-squaring.c

--- 1/36-squaring.c
+++ 1/36-squaring.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() 
+  
 {
-
+  printf("RA2211042010049\n");
+  
   int i,square; 
   printf("enter a number");
   scanf("%d",&i);
   square=i*i;
   printf("%d",square);
   return 0;
-
 }
