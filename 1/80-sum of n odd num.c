fd91f8b88d13c75a04b631f1aafc56b1d04201c1
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 17:02:42 +0530

    Update and rename sum of n odd num.c (80) to 80-sum of n odd num.c

--- 1/80-sum of n odd num.c
+++ 1/80-sum of n odd num.c
@@ -5,9 +5,11 @@ int main()
   printf("Enter n value: ");
   scanf("%d",&n);
   for(int i=1; i<=n; i++)
+    
   {
     if(i%2!=0) sum += i;
   }
+  
   printf("Sum of odd numbers from 1 to %d is: %d", n, sum);
   return 0; 
 }
