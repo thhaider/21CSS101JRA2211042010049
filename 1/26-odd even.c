6a797a575720e7bf35cbf322f8d8440f98cde229
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 17:35:47 +0530

    Update and rename odd even.c (26) to 26-odd even.c

---
+++ 1/26-odd even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    printf("RA2211042010049\n");
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    printf("%d is even.", num);
+    else
+    printf("%d is odd.", num);
+    return 0;
+}
