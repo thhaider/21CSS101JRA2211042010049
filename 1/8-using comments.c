9129c1290ae72259c960c40521921a80dab6bf98
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 16:59:24 +0530

    Update and rename using comments.c (8) to 8-using comments.c

--- 1/8-using comments.c
+++ 1/8-using comments.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() 
 {
+printf("RA2211042010049\n");
+  
 printf("Hello Github !");
 // this is a comment used for explaining the code  , comments are not part of the code 
 return 0;
