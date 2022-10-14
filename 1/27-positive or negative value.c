c187834e8a0f9a26828b29729214bd0555455fe1
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 17:27:01 +0530

    Update and rename positive or negative value.c (27) to 27-positive or negative value.c

---
+++ 1/27-positive or negative value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int myNum = 10;
+  if (myNum > 0)
+  printf("The value is a positive number.");
+  else if (myNum < 0)
+  printf("The value is a negative number.");
+  else
+  printf("The value is 0."); 
+  return0;
+}
