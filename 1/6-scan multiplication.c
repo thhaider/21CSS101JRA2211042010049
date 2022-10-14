03c1e2dd010a9498156453d314165cc60d39b783
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 17:13:15 +0530

    Update and rename scan multiplication.c(6) to 6-scan multiplication.c

--- 1/6-scan multiplication.c
+++ 1/6-scan multiplication.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main ()
 {
+    printf("RA2211042010049\n");
+    
     int a,b,c;
-    printf("Enter value of A = \n");
+    printf("Enter value of a = \n");
     scanf("%d",&a);
     printf("Enter value of b = \n");
     scanf("%d",&b);
-    
     printf("A * B = %d", a*b);
     return 0;
 }
