515115b99095f5db1bd1a3ffe92d131267eb65d4
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 18:09:55 +0530

    Update and rename if else.c (98) to 98-if else.c

--- 1/98-if else.c
+++ 1/98-if else.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
   int time = 14;
-  if (time > 8) {
-    printf("Good day.");
-  } else {
+  if (time > 8) 
+  {
+  printf("Good day.");
+  } 
+  else 
+  {
     printf("Good evening.");
   }
   return 0;
