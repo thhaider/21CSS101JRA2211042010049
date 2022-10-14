dd395337b5cdc590aa8f8ef4bcf10e27217ffa75
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-14 16:58:39 +0530

    Update and rename value of quotient and remainder .c (52) to 52-value of quotient and remainder.c

--- 1/52-value of quotient and remainder.c
+++ 1/52-value of quotient and remainder.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 int main()
 {
+    printf("RA2211042010049\n");
+    
     int dividend, divisor, quotient, remainder;
     printf("Enter dividend: ");
     scanf("%d", &dividend);
     printf("Enter divisor: ");
     scanf("%d", &divisor);
-
-    // For Computing quotient
-    quotient = dividend / divisor;
-
-    // For Computing remainder
-    remainder = dividend % divisor;
-
     printf("Quotient = %d\n", quotient);
     printf("Remainder = %d", remainder);
     return 0;
