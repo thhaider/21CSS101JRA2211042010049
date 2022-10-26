7f203f35b5d6f9dcf17c238f758fdbbf4cddeff5
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-10-26 09:57:48 +0530

    Create 79- for ,weight and height.c

---
+++ 1/79- for ,weight and height.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+  int count, i;
+  float weight, height;
+  
+  count = 0;
+  printf("Enter the weight and height for 5 boys\n");
+  
+  for(i=1; i<=5; i++)
+    {
+    scanf("%f %f", &weight, &height);
+    if (weight<50 && height>170)
+    count = count + 1;
+    }
+    
+    printf("Number of boys with weight < 50kg\n");
+    printf("and height > 170 cm = %d\n", count);
+}
