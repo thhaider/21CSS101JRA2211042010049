62582fa83dfb7aa7e607878a62e298628c4821ce
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 20:49:12 +0530

    Update 2.c

--- 1/CD/Unit 2/Lesson 1/2.c
+++ 1/CD/Unit 2/Lesson 1/2.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-void main() {
+void main() 
+{
+	printf("RA2211042010049\n");
 	int num;
 	printf("Enter a number : ");
 	scanf("%d", &num);
-	if (num % 3 == 0) {  
-		printf("Given number %d is divisible by 3\n", num);
+	if (num % 3 == 0) 
+	{  
+	printf("Given number %d is divisible by 3\n", num);
 	}
 }
