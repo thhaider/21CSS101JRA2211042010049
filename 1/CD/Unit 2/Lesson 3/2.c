83f8472c1a9efe186104836f503614523e392366
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 20:54:52 +0530

    Update 2.c

--- 1/CD/Unit 2/Lesson 3/2.c
+++ 1/CD/Unit 2/Lesson 3/2.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-void main() {
+void main() 
+{
+	printf("RA2211042010049\n");
 	char ch;
 	printf("Enter a character : ");
 	ch = getchar();
-	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+	{
 		printf("Given character %c is an alphabet\n", ch);
 	}
-	if (ch >= '0' && ch <= '9') {
+	if (ch >= '0' && ch <= '9') 
+	{
 		printf("Given character %c is a digit\n", ch);
 	}
 }
