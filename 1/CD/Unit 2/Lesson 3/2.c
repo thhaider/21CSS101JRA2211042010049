27d8df5c5629899610ac72e4184e77c88277ff8b
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 12:56:04 +0530

    Create 2.c

---
+++ 1/CD/Unit 2/Lesson 3/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+	char ch;
+	printf("Enter a character : ");
+	ch = getchar();
+	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+		printf("Given character %c is an alphabet\n", ch);
+	}
+	if (ch >= '0' && ch <= '9') {
+		printf("Given character %c is a digit\n", ch);
+	}
+}
