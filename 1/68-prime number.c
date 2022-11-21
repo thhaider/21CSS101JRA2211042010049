9947436ad1d8d5e49bb127340602b26020f081cb
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 20:41:43 +0530

    Update 68-prime number.c

--- 1/68-prime number.c
+++ 1/68-prime number.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main()
 {
+  printf("RA2211042010049\n");
   int n, i, flag = 0;
   printf("Enter a positive integer: ");
   scanf("%d", &n);
   if (n == 0 || n == 1)
   flag = 1;
   for (i = 2; i <= n / 2; ++i) 
-  {
+ {
     if (n % i == 0) 
-    {
-    flag = 1;
-    break;
-    }
-  }
+ {
+ flag = 1;
+ break;
+ }
+ }
   if (flag == 0)
   printf("%d is a prime number.", n);
   else
