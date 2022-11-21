c86470f2afda5857d46fe6531f47c1d01f45dd50
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 10:08:10 +0530

    Create 1.c

---
+++ 1/CD/Unit 2/Lesson 2/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+int distinction_marks = 75;
+if (marks > distinction_marks) {
+	printf("User secured distinction.\n");
+} else {
+	printf("User did not secure distinction.\n");
+}
