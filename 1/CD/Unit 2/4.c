89c387c702246d9feb5483af392981b30de83df4
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-21 10:03:09 +0530

    Create 4.c

---
+++ 1/CD/Unit 2/4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() {
+    int a = 3;
+    float b = 3.0;
+    if (a == b)
+        printf("a and b are equal");
+    if (a != b)
+        printf("a and b are not equal"); 
+}
