c596d7849e8e5cd58115742de0e9f1319eb7e8ff
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-24 01:51:48 +0530

    Create CT1(2).c

---
+++ 1/CT1(2).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+printf("RA2211042010049\n");
+printf("Ct question : Find the output for the following c program?\n");
+int a=5;
+int __=10;
+int ___;
+___=a+__;
+printf("%i", ___);
+return 0;
+}
