e9dbd4f5748e69f6b0d6f78bd581fb6a469b4f49
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-24 01:53:06 +0530

    Create CT1(3).c

---
+++ 1/CT1(3).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("RA2211042010049\n");
+printf("CT Find out the value of s, after evaluating the following expression.\n");
+int s, qui = 4, add = 2, god = 2;
+s=qui*add/4-6/2+2/3*6/god;
+printf("s=%d", s);
+return 0;
+}
