feff4ae6fa40a3dbf3c3f057a6683ed796d2e4cb
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-25 00:19:11 +0530

    Create 12.c

---
+++ 1/CD/Unit 3/12.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <math.h>
+void main() 
+{
+  printf("RA2211042010049\n");
+	int x;
+	float y;
+	printf("Enter the value : ");
+	scanf("%d", &x);
+	y = sqrt(x);
+	printf ("The square root of %d is : %f", x, y);
+}
