f2267cb23d4b20fb8e91e9655df19cc32020f6c6
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-25 00:10:11 +0530

    Create 6.c

---
+++ 1/CD/Unit 3/6.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <string.h>
+void main(void)
+{
+  printf("RA2211042010049\n");
+	char str1[10] = "Hello";
+	char str2[10] = "Students";
+	strcpy(str1, "Hi");
+	printf("%s %s", str1, str2);
+ }
