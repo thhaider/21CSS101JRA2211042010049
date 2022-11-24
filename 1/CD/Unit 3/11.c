794d65256a5d0dfe15d4dcd3bf86575ac469a611
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-11-25 00:16:56 +0530

    Create 11.c

---
+++ 1/CD/Unit 3/11.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+void main(void) 
+{
+  printf("RA2211042010049\n");
+	char str1[] = "Hello";
+	char str2[] = "Hello";
+	if (str1 == str2)
+		printf("Equal\n");
+	else
+		printf("Unequal\n");
+}
