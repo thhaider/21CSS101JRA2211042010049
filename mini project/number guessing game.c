f3d75daa709f4dff82fd4459d4de1f37b82b034b
Haider Shah <113873489+thhaider@users.noreply.github.com>
2022-12-26 13:01:25 +0530

    Update number guessing game.c

--- mini project/number guessing game.c
+++ mini project/number guessing game.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<stdlib.h>  //used to generated random number
-#include<time.h>    //used to generated random number
+#include<stdlib.h>  //used to generate a random number
+#include<time.h>    //used to generate a different random number every time 
 
 int main(){
     int number, guess, nguesses=1;
     srand(time(0));
     number = rand()%100 + 1; // Generates a random number between 1 and 100
-    // printf("The number is %d\n", number);
-    // Keep running the loop until the number is guessed
+    //printf("The number is %d\n", number);
+    // Keeps running the loop until the number is guessed
     do{
         printf("Guess the number between 1 to 100\n");
         scanf("%d", &guess);
